5d381c32e7fa8c4bc2734241d305f967f6cf6ecf
eriktx11 <erik@llerenapiano.com>
2016-11-26 09:56:49 -0600

    scene: Ball game

---
+++ Assets/scripts/adr_balance.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adr_balance : MonoBehaviour {
+
+	GameObject player;
+	Rigidbody rigidbody;
+	float popForce=100f;
+
+	void Start(){
+		rigidbody = GetComponent<Rigidbody> ();
+	}
+
+
+	void Update () {
+		//the more it taps the more it jumps
+		if(Input.GetMouseButtonDown(0)) 
+			rigidbody.AddForce(Vector3.up*popForce,ForceMode.Force);
+
+	}
+		
+}
